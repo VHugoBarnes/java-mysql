5495a60d59926bd9daed5a6d21d0c4d6530c4fae
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2023-12-19 21:06:55 -0600

    Find all and get by id functions implemented

--- src/main/java/dev/victorvazquez/Main.java
+++ src/main/java/dev/victorvazquez/Main.java
@@ -1,5 +1,8 @@
 package dev.victorvazquez;
 
+import dev.victorvazquez.model.Employee;
+import dev.victorvazquez.repository.EmployeeRepository;
+import dev.victorvazquez.repository.Repository;
 import dev.victorvazquez.util.DatabaseConnection;
 
 import java.sql.*;
@@ -7,12 +10,12 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        try(Connection myConn = DatabaseConnection.getInstance();
-            Statement myStamt = myConn.createStatement();
-            ResultSet myRes = myStamt.executeQuery("SELECT * FROM employees");) {
-            while(myRes.next()) {
-                System.out.println(myRes.getString("name"));
-            }
+        try(Connection myConn = DatabaseConnection.getInstance();) {
+            Repository<Employee> repository = new EmployeeRepository();
+
+            repository.findAll().forEach(System.out::println);
+            System.out.println("============================");
+            System.out.println(repository.getById(1));;
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("Something went wrong");
